a64d23c314a9e96720d755c231ac80d76b05a7bc
guelate <guelateseyo@gmail.com>
2024-10-14 10:27:04 +0200

    feat(client):support view complete

---
+++ client/app/contact/page.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable react/jsx-key */
+"use client";
+import { Github, Mail, Linkedin } from "lucide-react";
+import Link from "next/link";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+
+const socials = [
+  {
+    icon: <Linkedin size={20} />,
+    href: "https://www.linkedin.com/in/killian-seyo/",
+    label: "Linkedin",
+    handle: "@Killian SEYO",
+  },
+  {
+    icon: <Mail size={20} />,
+    href: "mailto:guelateseyo@gmail.com",
+    label: "Email",
+    handle: "guelateseyo@gmail.com",
+  },
+  {
+    icon: <Github size={20} />,
+    href: "https://github.com/guelate",
+    label: "Github",
+    handle: "Guelate",
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="bg-[#030712]">
+      <Navigation />
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+          {socials.map((s) =>
+            s.label == "Email" ? (
+              <Card>
+                <div className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16 bg-[#0a101f]">
+                  <span
+                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                    aria-hidden="true"
+                  />
+                  <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                    {s.icon}
+                  </span>
+                  <div className="z-10 flex flex-col items-center">
+                    <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+                      {s.handle}
+                    </span>
+                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                      {s.label}
+                    </span>
+                  </div>
+                </div>
+              </Card>
+            ) : (       
+              <Card>
+                <Link
+                  key={s.href}
+                  href={s.href}
+                  target="_blank"
+                  className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16 bg-[#0a101f]"
+                >
+                  <span
+                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                    aria-hidden="true"
+                  />
+                  <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                    {s.icon}
+                  </span>{" "}
+                  <div className="z-10 flex flex-col items-center">
+                    <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+                      {s.handle}
+                    </span>
+                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                      {s.label}
+                    </span>
+                  </div>
+                </Link>
+              </Card>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
